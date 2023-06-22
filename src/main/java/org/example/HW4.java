4ac160628778a2b20d323612b28e34ad62e29071
Artem Maklashev <artem@maklashev.ru>
2023-06-22 08:13:57 +0400

    HW done

--- src/main/java/org/example/HW4.java
+++ src/main/java/org/example/HW4.java
@@ -1,93 +1,125 @@
-package org.example;
+class Tree{
+    private Node root;
+    class Node{
+        int value;
+        Node left;
+        Node right;
 
-public class HW4 {
-    class Tree{
-        private Node root;
-        class Node{
-            int value;
-            Node left;
-            Node right;
+        //boolean color; // true = RED, false = BLACK
+        Color color;
+    }
 
-            //boolean color; // true = RED, false = BLACK
-            Color color;
+    enum Color{
+        BLACK,
+        RED
+    }
+
+    public void insert(int value){
+        if(root == null){
+            root = new Node();
+            root.value = value;
+        }else{
+            insert(root, value);
+            root = balance(root);
         }
+        root.color = Color.BLACK;
+    }
 
-        enum Color{
-            BLACK,
-            RED
-        }
 
-        public void insert(int value){
-            if(root == null){
-                root = new Node();
-                root.value = value;
-            }else{
-                insert(root, value);
-//                root = balance(root);
-            }
-            //root.color = Color.BLACK;
-            //root.color = false;
-        }
 
-        private void insert(Node node, int value){
-            if(node.value != value){
-                if(node.value < value){
-                    if(node.right == null){
-                        node.right = new Node();
-                        node.right.value = value;
-                        //node.right.color = RED;
-                    }else{
-                        insert(node.right, value);
-                    }
+    private void insert(Node node, int value){
+        if(node.value != value){
+            if(node.value < value){
+                if(node.right == null){
+                    node.right = new Node();
+                    node.right.value = value;
+                    node.right.color = Color.RED;
                 }else{
-                    if(node.left == null){
-                        node.left = new Node();
-                        node.left.value = value;
-                        //node.left.color = RED;
-                    }else{
-                        insert(node.left, value);
-                    }
+                    insert(node.right, value);
+                    node.right = balance(node.right);
+                }
+            }else{
+                if(node.left == null){
+                    node.left = new Node();
+                    node.left.value = value;
+                    node.left.color = Color.RED;
+                }else{
+                    insert(node.left, value);
+                    node.left = balance(node.left);
                 }
             }
         }
-
-        public Node find(int value){
-            return find(root, value);
-        }
-
-        private Node find(Node node, int value) {
-            if(node == null){
-                return null;
-            }
-            if(node.value == value){
-                return node;
-            }
-            if(node.value < value) {
-                return find(node.right, value);
-            }else{
-                return find(node.left, value);
-            }
-        }
     }
 
-    public class Main {
-        public static void main(String[] args) {
-//        HashMap map = new HashMap();
-//
-//        map.push(-1, 2);
-//        map.push(17, 3);
-//        map.push(3, 4);
-//
-//        System.out.println(map.find(-1));
-//        System.out.println(map.find(17));
-//        System.out.println(map.find(3));
-//        System.out.println(map.find(5));
-//
-//        map.remove(17);
-//        System.out.println(map.find(17));
+    private Node rightRotate(Node node){
+        Node newNode = node.left;
+        node.left = newNode.right;
+        newNode.right = node;
+        newNode.color = node.color;
+        node.color = Color.RED;
+        return newNode;
+    }
+    private Node leftRotate(Node node){
+        Node newNode = node.right;
+        node.right = newNode.left;
+        newNode.left = node;
+        newNode.color = node.color;
+        node.color = Color.RED;
+        return newNode;
+    }
+    private void swapColors(Node node){
+        node.color = Color.RED;
+        node.left.color = Color.BLACK;
+        node.right.color = Color.BLACK;
+    }
+    private Node balance(Node node){
+        boolean isBalanced = true;
+        do {
+            isBalanced = true;
+            if(node.left != null && node.left.color == Color.RED && (node.left.left !=null && node.left.left.color == Color.RED)){
+                node = rightRotate(node);
+                isBalanced = false;
+            }
+            if((node.right != null && node.right.color == Color.RED) && (node.left.color == Color.BLACK || node.left == null)){
+                node = leftRotate(node);
+                isBalanced = false;
+            }
+            if((node.left != null && node.left.color == Color.RED) && (node.right != null && node.right.color == Color.RED)){
+                swapColors(node);
+                isBalanced = false;
+            }
+        } while (!isBalanced);
+        return node;
+    }
 
+    public Node find(int value){
+        return find(root, value);
+    }
 
-
+    private Node find(Node node, int value) {
+        if(node == null){
+            return null;
+        }
+        if(node.value == value){
+            return node;
+        }
+        if(node.value < value) {
+            return find(node.right, value);
+        }else{
+            return find(node.left, value);
         }
     }
 }
+
+public class HW4 {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(8);
+        tree.insert(5);
+        tree.insert(10);
+        tree.insert(4);
+        tree.insert(3);
+    }
+}
+
+
